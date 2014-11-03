ea4935720f0f6c771171a5b1280f9be3dafc1e65
haowhen <hnnguy02@louisville.edu>
2014-11-02 19:06:07 -0500

    dd two “buttons” using Android core icons (Search and Add) to the Actionbar. Clicking Search should open a search fragment and replace the grid fragment Clicking Add should fire a new activity for Add (again, your activity should have the activity/fragment example) Make sure you manage the up navigation with the home icon (this is what happens when you click the logo on the top left corner with the arrow) See http://developer.android.com/training/implementing-navigation/ancestral.html

--- app/src/main/java/com/cis490/haonguyen/hw5_notmymusic/MainActivity.java
+++ app/src/main/java/com/cis490/haonguyen/hw5_notmymusic/MainActivity.java
@@ -2,12 +2,15 @@ package com.cis490.haonguyen.hw5_notmymusic;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.cis490.haonguyen.hw5_notmymusic.fragment.FragmentMain;
+import com.cis490.haonguyen.hw5_notmymusic.fragment.FragmentSearch;
 
 public class MainActivity extends FragmentActivity {
 
@@ -33,14 +36,35 @@ public class MainActivity extends FragmentActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		switch (id){
+			case android.R.id.home:
+				NavUtils.navigateUpFromSameTask(this);
+				return true;
+			case R.id.action_add: add(); return true;
+			case R.id.action_search: search(); return true;
+		}
+        /*
         if (id == R.id.action_settings) {
+
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        */
+		return super.onOptionsItemSelected(item);
     }
 
+	private void search() {
+		FragmentSearch frag = new FragmentSearch();
+		FragmentManager manager = getFragmentManager();
+		FragmentTransaction transaction = manager.beginTransaction();
+		transaction.replace(R.id.container,frag,"FragmentSearch");
+		transaction.commit();
+	}
+
+	private void add(){
+		startActivity(new Intent(getApplicationContext(), AddActivity.class));
+	}
 }
